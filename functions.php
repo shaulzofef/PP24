ad4cc3f378305d1577c92afd19b3e7a36b39a20a
shaulzofef <shaulzofef@gmail.com>
2023-08-12 23:35:40 +0300

    moved styles back

--- functions.php
+++ functions.php
@@ -20,8 +20,8 @@ add_action('init','sz_menus');
 
 //Load CSS
 function sz_register_styles(){
-	wp_enqueue_style('sz_css_normalize', get_template_directory_uri() . "/assets/css/normalize.css", array(), '1.0', 'all');
-	wp_enqueue_style('sz_css_style', get_template_directory_uri() . "/assets/css/style.css", array(), '1.0', 'all');
+	wp_enqueue_style('sz_css_normalize', get_template_directory_uri() . "/normalize.css", array(), '1.0', 'all');
+	wp_enqueue_style('sz_css_style', get_template_directory_uri() . "/style.css", array(), '1.0', 'all');
 	wp_enqueue_style('sz_css_font-awsome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css', array(), '6.4.0', 'all');
 }
 add_action('wp_enqueue_scripts', 'sz_register_styles');
